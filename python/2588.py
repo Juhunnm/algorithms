d71f8828835b7cfbfd5b7c5859bf674da3b230db
juhun <minjuhun1@gmail.com>
2026-01-24 18:50:00 +0900

    26.01.24

---
+++ python/2588.py
@@ -0,0 +1,18 @@
+A = input()
+B = input()
+# print(A)
+for i in range(2,-1,-1) :
+    print(int(B[i]) * int(A))
+
+print(int(A) * int(B))
+# for i in range(2,-1,-1) : 
+#     # print(A[i])
+#     print(result)
+#     for j in range(2,-1,-1) :
+#         print(i,j)
+#         print(int(B[i]) *int(A[j]))
+#         result += int(B[i]) * int(A[j])
+# print(result)
+#     for j in len(B) :
+#         result = A[i] * B[j]
+# print(result)
