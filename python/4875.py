3a34cc741506f2474e26aab90db73eff271f603a
juhun <minjuhun1@gmail.com>
2026-07-04 22:58:04 +0900

    26.07.04

---
+++ python/4875.py
@@ -0,0 +1,13 @@
+# 4875. [S/W 문제해결 기본] 5일차 - 미로 
+T = int(input())
+
+for tase_case in range():
+    size = int(input())
+    miro = [[] for _ in range(size)]
+
+    
+    
+    for i in range(size):
+        info = int(input()) 
+    
+    print(f"#{tase_case} {size}")
